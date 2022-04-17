4505c7df8876c72f13dfe66c374a4f0fcc3e2354
kadirkara22 <kadirkar2204@gmail.com>
2022-04-17 12:09:27 +0300

    etkinlik sahibi,katılımcı ve location listelendi

--- odev1/client/src/pages/Event/index.js
+++ odev1/client/src/pages/Event/index.js
@@ -8,6 +8,8 @@ import styles from './styles.module.css'
 import Item from 'antd/lib/list/Item';
 const { Title } = Typography;
 
+
+
 const Event = () => {
     const { id } = useParams();
     const { loading, error, data } = useQuery(GET_EVENT, {
@@ -22,23 +24,34 @@ const Event = () => {
     if (error) {
         return <div>Error: {error.message}</div>
     }
-    console.log(data)
+    console.log(data.event)
     const { event } = data
     return (
-        <div>
-            <Title level={3}>{event.title}</Title>
-            <div>
-                {event.location.name}
+        <>
+            <div className={styles.eventTitle}>
+                <span>Etkinlik sahibi</span>
+                <span>Adress</span>
+                <span>Katılımcılar</span>
             </div>
-            <div>
-                {event.user.username}
-            </div>
-            <div>
+            <div className={styles.eventContent}>
+                <div>
+                    {event.user.username}
+                </div>
+                <div>
+                    {event.location.name}
+                </div>
+
+                <div className={styles.eventParticipants}>
+                    {
+                        event.participants.map((item, i) => (
+                            <div className={styles.eventOneParticipant}> {item.user.username} </div>
+                        ))
+
+                    }
+                </div>
 
             </div>
-
-            {/* <Comments post_id={id} /> */}
-        </div>
+        </>
     )
 }
 
